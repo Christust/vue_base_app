1a4e23bb55316ca93aee38c2f1722cd56172a2aa
Moderantem <moderantem@gmail.com>
2024-01-28 21:24:41 -0600

    iconos tecnologias

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -17,5 +17,48 @@
       deploy. Asegurando que cada push a la rama principal haga un deploy de manera automatica
       (CI/CD).
     </p>
+
+    <div class="row justify-content-between aling-items-center">
+      <div class="card cardGradient col-1">
+        <img src="https://miro.medium.com/v2/resize:fit:600/1*R4c8lHBHuH5qyqOtZb3h-w.png" />
+      </div>
+      <div class="card cardGradient col-1">
+        <img src="https://bitperfect.at/assets/blog-images/Headerbild-Was-ist-GitHub-v2.png" />
+      </div>
+      <div class="card cardGradient col-1">
+        <img src="https://pinia.vuejs.org/logo.svg" />
+      </div>
+      <div class="card cardGradient col-1">
+        <img
+          class="p-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
+        />
+      </div>
+      <div class="card cardGradient col-1">
+        <img
+          class="p-1"
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg"
+        />
+      </div>
+      <div class="card cardGradient col-1">
+        <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/axios_logo_icon_168545.png" />
+      </div>
+      <div class="card cardGradient col-1">
+        <img
+          src="https://appmarket.unbounce.com/uploads/application/logos/phpy6zmkz-62e0093d9c997.svg"
+        />
+      </div>
+      <div class="card cardGradient col-1">
+        <img
+          src="https://user-images.githubusercontent.com/7110136/29002858-a09570d2-7ab4-11e7-8faa-5dd6d4458b0d.png"
+        />
+      </div>
+      <div class="card cardGradient col-1">
+        <img
+          class="p-1"
+          src="https://raw.githubusercontent.com/jpb06/jpb06/master/icons/GithubActions-Dark.svg"
+        />
+      </div>
+    </div>
   </div>
 </template>
